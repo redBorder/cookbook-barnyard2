ae4529d9b005ada85ca48bc102b21cacdce4d38b
Miguel <manegron@redborder.com>
2022-03-31 16:15:13 +0100

    Working on first version

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -13,80 +13,129 @@ action :add do #Usually used to install and configure something
     templates_dir = new_resource.templates_dir
     user = new_resource.user
     sensors = new_resource.sensors
+    
+    groups = node["redborder"]["snort"]["groups"].keys.map{|x| x.to_i}
+    used_segments = []
 
-    chef_gem 'ruby_dig' do
-      action :nothing
-    end.run_action(:install)
+    node["redborder"]["snort"]["groups"].each do |id, original_group|
+      group = node["redborder"]["snort"]["groups"][id].to_hash.clone
+      bindings = group["bindings"].keys.map{|x| x.to_i}.flatten
+      name = (group["name"].nil? ? "default" : group["name"].to_s)
+      group["name"]     = name 
+      group["segments"] = node["redborder"]["segments"].keys  if group["segments"].nil?
+      group["cpu_list"] = 0.upto(node["cpu"]["total"]-1).to_a if group["cpu_list"].nil?
+      group["cpu_list"] = (group["cpu_list"].map { |x| x.to_i }).sort.uniq
+      group["segments"] = group["segments"].sort.uniq
+      group["instances_group"] = id.to_i
+      group["segments"] = group["segments"].select{ |x| !used_segments.include?(x) }
+  
+      if group["cpu_list"].size==0 or group["segments"].size==0 or group["bindings"].nil? or group["bindings"].size==0
+        groups.delete(id.to_i)
+      else
+        used_segments = used_segments + group["segments"]
+        
+    
+        [ "barnyard2" ].each do |s|
+          [ "reload", "restart", "stop", "start" ].each do |s_action|
+            execute "#{s_action}_#{s}_#{group["instances_group"]}_#{name}" do
+              command "/bin/env WAIT=1 /etc/init.d/#{s} #{s_action} #{name}" 
+              ignore_failure true
+              action :nothing
+            end
+          end
+        end
+  
+        template "/etc/snort/#{group["instances_group"]}/barnyard2.conf" do
+          source "barnyard2.conf.erb"
+          cookbook "barnyard2"
+          owner "root"
+          group "root"
+          mode 0644
+          retries 2
+          variables(:sensor_id => sensor_id, :name => name, :group => group)
+          notifies :run, "execute[restart_barnyard2_#{group["instances_group"]}_#{name}]", :delayed
+        end
+  
+       
+        template "/etc/sysconfig/barnyard2-#{group["instances_group"]}" do
+          source "barnyard2.erb"
+          cookbook "barnyard2"
+          owner "root"
+          group "root"
+          mode 0644
+          retries 2
+          variables(:sensor_id => sensor_id, :name => name, :group => group)
+          notifies :run, "execute[restart_barnyard2_#{group["instances_group"]}_#{name}]", :delayed
+        end
+      end
+    end
 
-    #User creation
-    user user do
+
+    known_ips={}
+    node_keys = Chef::Node.list.keys.sort
+    node_keys.each do |n_key|
+      n = Chef::Node.load n_key
+      known_ips[n["rbname"]] = n["ipaddress"] if !n["ipaddress"].nil? and !n["rbname"].nil?
+    end
+    
+    directory "/etc/objects" do
+      owner "root"
+      group "root"
+      mode 0774
       action :create
     end
-
-    # Directory creation
-    directory config_dir do
-      owner "root"
-      group "root"
-      mode 0755
+    
+    [ "icmps", "interfaces", "hosts" ].each do |ob|
+      objects={}
+      if (!node["redborder"].nil? and !node["redborder"]["objects"].nil? and !node["redborder"]["objects"][ob].nil? and node["redborder"]["objects"][ob].class != Chef::Node::ImmutableArray)
+        if ob=="hosts"
+            objects = known_ips.merge(node["redborder"]["objects"][ob])
+          else
+            objects=node["redborder"]["objects"][ob]
+          end
+      elsif ob=="hosts"
+        objects=known_ips
+      end
+  
+      template "/etc/objects/#{ob}" do
+        source "objects_hosts.erb"
+        cookbook "barnyard2"
+        owner "root"
+        group "root"
+        mode 0644
+        retries 2
+        variables(:objects => objects)
+        notifies :reload, "service[barnyard2]", :delayed
+      end
+    end
+    
+    [ "macs", "protocols", "services", "networks", "vlans" ].each do |ob|
+      ob_db = Chef::DataBagItem.load('rBglobal', ob) rescue ob_db=nil
+      objects={}
+      if !ob_db.nil? and !ob_db["list"].nil? and !ob_db["list"].empty?
+          objects=ob_db["list"]
+      end
+    
+      template "/opt/rb/etc/objects/#{ob}" do
+        source "objects.erb"
+        cookbook "barnyard2"
+        owner "root"
+        group "root"
+        mode 0644
+        retries 2
+        variables(:objects => objects)
+        notifies :reload, "service[barnyard2]", :delayed
+      end
     end
 
-    directory cache_dir do
-      owner user
-      group user
-      mode 0755
-    end
-
-    directory templates_dir do
-      owner user
-      group user
-      mode 0755
-    end
-
-
-    # RPM Installation
-    yum_package "barnyard2" do
-      action :upgrade
-    end
-
-    # Memory calculation
-    dns_cache_size_mb = [ memory_kb/(4*1024), 10 ].max.to_i
-    buffering_max_messages = [ memory_kb/4, 1000 ].max.to_i
-
-    # Templates
-    template "/etc/sysconfig/barnyard2" do
-      source "barnyard2_sysconfig.erb"
-      cookbook "barnyard2"
-      owner "root"
-      group "root"
-      mode 0644
-      retries 2
-      variables(  :cores => cores,
-                  :enrichment_enabled => enrichment_enabled,
-                  :cache_dir => cache_dir,
-                  :config_file => "#{config_dir}/config.json",
-                  :dns_cache_size_mb => dns_cache_size_mb,
-                  :user => user,
-                  :buffering_max_messages => buffering_max_messages
-      )
-      notifies :reload, 'service[barnyard2]', :delayed
-    end
-
-    template "#{config_dir}/config.json" do
-      source "barnyard2_config.erb"
-      cookbook "barnyard2"
-      owner "root"
-      group "root"
-      mode 0644
-      retries 2
-      variables(:sensors => sensors)
-      helpers Barnyard2::Renderer
-      notifies :reload, 'service[barnyard2]', :delayed
-    end
 
     service "barnyard2" do
-      supports :status => true, :start => true, :restart => true, :reload => true, :stop => true
-      action [:enable, :start]
+      #service_name node[:redBorder][:barnyard2][:service]
+      ignore_failure true
+      supports :status => true, :reload => true, :restart => true
+      action([:start, :enable])
     end
+
 
     Chef::Log.info("barnyard2 cookbook has been processed")
   rescue => e
@@ -102,45 +151,6 @@ action :remove do #Usually used to uninstall something
     end
 
     Chef::Log.info("barnyard2 cookbook has been processed")
-  rescue => e
-    Chef::Log.error(e.message)
-  end
-end
-
-action :register do #Usually used to register in consul
-  begin
-    if !node["barnyard2"]["registered"]
-      query = {}
-      query["ID"] = "barnyard2-#{node["hostname"]}"
-      query["Name"] = "barnyard2"
-      query["Address"] = "#{node["ipaddress"]}"
-      query["Port"] = 2055
-      json_query = Chef::JSONCompat.to_json(query)
-
-      execute 'Register service in consul' do
-        command "curl -X PUT http://localhost:8500/v1/agent/service/register -d '#{json_query}' &>/dev/null"
-        action :nothing
-      end.run_action(:run)
-
-      node.set["barnyard2"]["registered"] = true
-    end
-    Chef::Log.info("barnyard2 service has been registered in consul")
-  rescue => e
-    Chef::Log.error(e.message)
-  end
-end
-
-action :deregister do #Usually used to deregister from consul
-  begin
-    if node["barnyard2"]["registered"]
-      execute 'Deregister service in consul' do
-        command "curl -X PUT http://localhost:8500/v1/agent/service/deregister/barnyard2-#{node["hostname"]} &>/dev/null"
-        action :nothing
-      end.run_action(:run)
-
-      node.set["barnyard2"]["registered"] = false
-    end
-    Chef::Log.info("barnyard2 service has been deregistered from consul")
   rescue => e
     Chef::Log.error(e.message)
   end
